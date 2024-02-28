e193d26bf757054153cd58d0129dfc3686196644
Vijay Vikram Singh <svijayvikram00@gmail.com>
2024-02-28 17:52:57 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Vijay Singh on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
